456024774ac485f705bef58e0ef3819c1b6ba53e
Slaven Tomac <slaven.tomac@gmail.com>
2019-07-05 14:39:17 +0200

    Added support for newest angular 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { NgTsSpinnerModule } from '../ng-ts-spinner/ng-ts-spinner.module';
@@ -13,7 +12,6 @@ import { NgTsSpinnerModule } from '../ng-ts-spinner/ng-ts-spinner.module';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     NgTsSpinnerModule
   ],
   providers: [],
